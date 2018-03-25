162570c0c5557de12e94be673cddb48259032851
nambiho.moon <nambiho.moon@gmail.com>
2018-03-25 14:46:11 +0900

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log(bundles)
+const http = require('http');
+const fs = require('fs');
+
+
+function createServer (app, port) {
+	let server = http.createServer(app);
+	server.listen(port);
+	console.log('server started')
+}
+
+
+
+createServer((req, res) => {
+	const {headers, method, url} = req;
+	if (url === '/') {
+		fs.readFile(__dirname + '/index.html', (err, data) => {
+			if (err) {
+				res.writeHead(404, {'Content-Type':'text/html'});
+				res.write('<h1>' + err + '</h1>');
+				res.end();
+				return;
+			}
+			res.writeHead(200, {'Content-Type':'text/html'});
+			res.write(data);
+			res.end()
+		});
+	}
+
+	if (url.indexOf('/public') !== -1) {
+		fs.readFile(__dirname + url, (err, data) => {
+			if (err) {
+				res.writeHead(404, {'Content-Type':'text/html'});
+				res.write('<h1>' + err + '</h1>');
+				res.end();
+				return;
+			}
+			res.writeHead(200, {'Content-Type':'text/javascript'});
+			res.write(data);
+			res.end()
+		});
+	}
+}, 8000)
