f008ab40c21777fc5120e15ca787e1f5197fc0c4
nambiho.moon <nambiho.moon@gmail.com>
2018-03-27 14:22:54 +0900

    simplelib.js

--- src/simplelib.js
+++ src/simplelib.js
@@ -1,6 +1,13 @@
-class simplelib {
-	constructor() {
-		console.log(1)
+import {bInfo} from '../lib/navigator';
+
+
+class Simplelib {
+	constructor (opt) {
+		//console.log(bInfo)
+		this.bInfo = bInfo
 	}
 }
 
+export default function simplelib (opt) {
+	return new Simplelib(opt)
+}
