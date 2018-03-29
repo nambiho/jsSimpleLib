0115f6f03dc715cc0b4703b2b86cece7606db291
nambiho.moon <nambiho.moon@gmail.com>
2018-03-29 13:21:16 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ import simplelib from './simplelib';
 
 window.addEventListener('DOMContentLoaded', function (ev) {
 	var sl = new simplelib({});
-	console.log(sl.bInfo())
+
+	console.log(sl.util.delPrefixMerge({a:'1'}, 'aaa', {'_b':'2'}));
+
+	//sl.util.merge()
 });
