6408ca36cfa739b6cefe0f8c679ee0c543ca75cc
nambiho.moon <nambiho.moon@gmail.com>
2018-03-31 20:29:35 +0900

    app.js

--- app.js
+++ app.js
@@ -40,6 +40,21 @@ createServer((req, res) => {
 		});
 	}
 
+
+	if (url.indexOf('/build') !== -1) {
+		fs.readFile(__dirname + url, (err, data) => {
+			if (err) {
+				res.writeHead(404, {'Content-Type':'text/html'});
+				res.write('<h1>' + err + '</h1>');
+				res.end();
+				return;
+			}
+			res.writeHead(200, {'Content-Type':'text/javascript'});
+			res.write(data);
+			res.end()
+		});
+	}
+
 	if (url.indexOf('/lib') !== -1) {
 		fs.readFile(__dirname + url, (err, data) => {
 			if (err) {
