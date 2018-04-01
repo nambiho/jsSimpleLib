8fb4ad457f9e2b5933e2fc00b0e76a68e40830aa
nambiho.moon <nambiho.moon@gmail.com>
2018-04-01 13:49:21 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', function (ev) {
 	var sl = new simplelib({});
 
 	//console.log(sl.util.delPrefixMerge({a:'1'}, 'aaa', {'_b':'2'}));
-	console.log(sl.util.getbyTagName('div'))
+	//console.log(sl.util.getbyTagName('div'))
 	//sl.util.merge()
+	console.log(sl)
 });
