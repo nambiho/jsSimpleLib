c074d137bb0fb6f662e26766a4291a12a367567f
nambiho.moon <nambiho.moon@gmail.com>
2018-04-02 02:27:24 +0900

    task.js

--- src/task.js
+++ src/task.js
@@ -7,10 +7,17 @@ export const Runtask = function(_t) {
 	{
 		async:true/false
 		func:function
-		object:parent
-		argv:function arguments
+		object: thisArg
+		argv:function argument array
 	}
 	*/
+
+
+	const prms = new Promise(function (resolve, reject) {
+
+	});
+
+
 	var _this,__queue;
 	return _this = this,
 	__queue=_t || [],
@@ -28,9 +35,11 @@ export const Runtask = function(_t) {
 				__f = _.func,
 				__o = _.object||null,
 				__a = (isArray(_.argv) ? _.argv : [_.argv]);
-				isFunction(__f) && (__s ? setTimeout(function(){
-					__f.apply(__o || _this, __a)
-				},0) : __f.apply(__o || _this, __a))
+				isFunction(__f) && 
+				(__s 
+					? setTimeout(function(){__f.apply(__o || _this, __a)},0)
+					: __f.apply(__o || _this, __a)
+				)
 			})
 		}
 	}
