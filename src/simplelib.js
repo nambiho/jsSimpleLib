fcfcf1156484ed7b387cb4ca286073d4ed7235a9
nambiho.moon <nambiho.moon@gmail.com>
2018-04-01 13:49:34 +0900

    simplelib.js

--- src/simplelib.js
+++ src/simplelib.js
@@ -1,15 +1,20 @@
 
-import Version from './version';
+import {Version} from './version';
 import {bInfo} from './navigator';
-import {default as util} from './utillity.js';
+import util from './utillity.js';
+import {Runtask} from './task.js';
+import {loader} from './loader.js';
 
 export default class simplelib {
 	constructor (opt) {
 		//console.log(bInfo)
 		this.bInfo = bInfo();
 		this.version = Version;
-		this.option = opt
+		this.option = opt;
+		this.runtask = Runtask;
+		this.loader = loader
 	}
 }
 
 simplelib.prototype.util = util();
+
