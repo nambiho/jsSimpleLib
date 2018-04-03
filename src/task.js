fcf54f52aa58a06419c3c06c16d3b47c4573f371
nambiho.moon <nambiho.moon@gmail.com>
2018-04-03 14:39:46 +0900

    task.js

--- src/task.js
+++ src/task.js
@@ -1,45 +1,46 @@
 'use strict';
 
-import {default as util} from './utillity.js';
+//import util from './utillity.js';
 
-export const Runtask = function(_t) {
+export const Runtask = function(taskInfo) {
 	/*
-	{
+	taskInfo = {
 		async:true/false
 		func:function
 		object: thisArg
 		argv:function argument array
 	}
 	*/
+	const promise = new Promise(function (resolve, reject) {}),
+	_this = this;
+
+	let queue = [];
+	
+	if (this.util.isArray(taskInfo)) {
+		queue = queue.concat(taskInfo)
+	} else if (this.util.isJSON(taskInfo)) {
+		queue = queue.concat([taskInfo])
+	} else return {run:this.util.noop}
 
 
-	const prms = new Promise(function (resolve, reject) {
-
+	queue = queue.filter(function (entry) {
+		return ('func' in entry)
 	});
 
+	return {
+		run : function () {
+			queue.forEach(function (entry) {
+				const async = entry.async,
+				func = entry.func,
+				object = entry.object||null,
+				argv = (_this.util.isArray(entry.argv) ? entry.argv : [entry.argv]);
 
-	var _this,__queue;
-	return _this = this,
-	__queue=_t || [],
-	{
-		add : function (_) {
-			if (!isArray(_)) return;
-			var _e=_.every(function (_1,_2) {
-				return ("func" in _1)
-			});
-			_e&&(__queue=__queue.concat(_))
-		},
-		run : function(){
-			__queue.forEach(function (_) {
-				var __s = _.async,
-				__f = _.func,
-				__o = _.object||null,
-				__a = (isArray(_.argv) ? _.argv : [_.argv]);
-				isFunction(__f) && 
-				(__s 
-					? setTimeout(function(){__f.apply(__o || _this, __a)},0)
-					: __f.apply(__o || _this, __a)
-				)
+				if (_this.util.isFunction(func)) {
+					if (async)
+						setTimeout(function(){func.apply(object || _this, argv)}, 0)
+					else
+						func.apply(object||_this, argv)
+				}
 			})
 		}
 	}
