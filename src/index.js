eaf8823122f02b4a55310cb51796b141af73ba59
nambiho.moon <nambiho.moon@gmail.com>
2018-04-04 10:11:13 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ window.addEventListener('DOMContentLoaded', function (ev) {
 	//console.log(sl.util.delPrefixMerge({a:'1'}, 'aaa', {'_b':'2'}));
 	//console.log(sl.util.getbyTagName('div'))
 	//sl.util.merge()
-	sl.runTask([{
+
+	/* runtask exam */
+	/*
+	var tasks = [{
 		func : function () {
 			var i = 0;
 			for (; i < 100 ; i++) {}
@@ -19,5 +22,19 @@ window.addEventListener('DOMContentLoaded', function (ev) {
 	},{
 		func : function () {console.log('sync = true')},
 		object : sl
-	}]).run();
+	}];
+
+	
+	var runtask = sl.runTask(tasks).add({
+		func:sl.util.noop
+	}).run({
+		func:function(){console.log(99);}
+	});
+
+	console.log(runtask);
+	console.log(runtask.add({func:sl.util.noop}))
+	*/
+	
+
+	console.log(sl);
 });
