3d14ff0a83106efbcbfc90a17bbddd718d55373d
nambiho.moon <nambiho.moon@gmail.com>
2018-04-04 10:11:30 +0900

    task.js

--- src/task.js
+++ src/task.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//import util from './utillity.js';
-
 export const Runtask = function(taskInfo) {
 	/*
 	taskInfo = {
@@ -11,37 +9,62 @@ export const Runtask = function(taskInfo) {
 		argv:function argument array
 	}
 	*/
-	const promise = new Promise(function (resolve, reject) {}),
-	_this = this;
 
-	let queue = [];
-	
-	if (this.util.isArray(taskInfo)) {
-		queue = queue.concat(taskInfo)
-	} else if (this.util.isJSON(taskInfo)) {
-		queue = queue.concat([taskInfo])
-	} else return {run:this.util.noop}
+	const THIS=this/*simplelib*/;
 
+	let QUEUE = [], oldQUEUE = [];
 
-	queue = queue.filter(function (entry) {
-		return ('func' in entry)
-	});
-
-	return {
-		run : function () {
-			queue.forEach(function (entry) {
-				const async = entry.async,
-				func = entry.func,
-				object = entry.object||null,
-				argv = (_this.util.isArray(entry.argv) ? entry.argv : [entry.argv]);
-
-				if (_this.util.isFunction(func)) {
-					if (async)
-						setTimeout(function(){func.apply(object || _this, argv)}, 0)
-					else
-						func.apply(object||_this, argv)
-				}
-			})
+	const getTasks = (tasks) => {
+		let tmp = [];
+		if (THIS.util.isArray(tasks)) {
+			tmp = tmp.concat(tasks)
+		} else if (THIS.util.isJSON(tasks)) {
+			tmp = tmp.concat([tasks])
 		}
-	}
+		tmp = tmp.filter(function (entry) {
+			return ('func' in entry)
+		});
+		return tmp;
+	};
+
+	const object = THIS.util.object({
+		add : function (tasks) {
+			let tmp = getTasks(tasks);
+			QUEUE = QUEUE.concat(tmp);
+			return this
+		},
+		run : function (tasks) {
+			tasks && this.add(tasks);
+			let proc, async, func, object, argv;
+
+			while (1) {
+				if (QUEUE.length === 0) break;
+				proc = QUEUE.splice(0,1)[0],
+				async = proc.async,
+				func = proc.func,
+				object = proc.object||null,
+				argv = (THIS.util.isArray(proc.argv) ? proc.argv : [proc.argv]);
+				if (THIS.util.isFunction(func)) {
+					(async)
+					? setTimeout(function(){func.apply(object, argv)}, 0)
+					: func.apply(object, argv)
+				}
+			}
+			return this
+		}
+	});
+	Object.defineProperty(object, 'queue', {
+		set : function (tasks) {
+			// set 함수에서 직접 변수를 변경 해야 한다.
+			// 다른 함수를 호출 하여 다른 함수에서 변수를 변경 하게 되면 재사용이 안됨
+			QUEUE = getTasks(tasks)
+		},
+		get : function () {
+			return QUEUE
+		}
+	});
+	Object.freeze(object);
+
+	object.queue = taskInfo;
+	return object
 }
