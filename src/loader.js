f4db28e607f7faaf0fb62a365cac2e68ba2306cb
nambiho.moon <nambiho.moon@gmail.com>
2018-04-08 02:10:47 +0900

    loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,36 +1,42 @@
 'use strict';
 
 export const loader = function (...argv){
-	/*
-	arg = [], ..., function
-	*/
+	/**
+	 * Worker does not support.
+	 * The only parent element is document.head.
+	 * 
+	 * arg = [], ..., function
+	 */
 
+	
 	let train = Array();
 	argv.forEach((entry, idx) => {
 		if (this.util.isArray(entry)) {
 			train=train.concat(entry)
 		} else train.push(entry)
 	});
-
-	//let idx = 0;
-	const promise = new Promise((resolve, reject) => {});
-	const location_src = (src) => {return src};
-	const onload = (id) => {}
+	const location_src = (src,cache) => {
+		let returnVal = src, d=/\?/.test(src);
+		returnVal += (cache? (d?'&':'?') + 't='+this.util.dateFormat(new Date(), 'yyyymmddss'):'');
+		return returnVal
+	};
+	const onload = (e) => {}
 	const load = (src) => {
 		if (!src) return;
-		const head = document.head;
 		const script = this.util.createElement({
 			dom:"script",
 			attr:{
-				src:location_src(src)
+				src:location_src(src,true),
+				charset:'utf-8',
+				type:'text/javascript'
 			},
 			event:{
-				error:function () {
+				error:function (e) {
 					throw new Error(src + ' : script loading error')
 				}
-			}
-		});
-		head.appendChild(script);
+			},
+			parent:document.head
+		})
 	};
 
 	train.forEach((entry, idx) => {
