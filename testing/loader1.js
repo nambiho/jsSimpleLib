5f2ca5b4d26c4e098675805160beeaabbbeedb78
nambiho.moon <nambiho.moon@gmail.com>
2018-04-12 01:44:57 +0900

    loader1.js

--- testing/loader1.js
+++ testing/loader1.js
@@ -1,12 +1,12 @@
 function loader1 () {
 	var sl = new simplelib();
-	var simpletest2 = sl.util.getbyId('simpletest2');
-	var loader1_text = 'loader1 text';
+	var loader_method = sl.util.getbyId('loader_method');
+	var loader1_text = 'simplelib.loader(\'loader1.js\')';
 	
 	var interval = 60, prev;
 	loader1_text.split('').forEach(function(val, idx){
 		var tm = setTimeout(function () {
-			simpletest2.textContent += val;
+			loader_method.textContent += val;
 			clearTimeout(tm)
 		}, (sl.util.isFalse(prev) ? 0 : 60) * idx);
 		prev = val
