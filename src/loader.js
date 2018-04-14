76230274966691a52dcbae6540f10b2d5b960d53
nambiho.moon <nambiho.moon@gmail.com>
2018-04-14 12:42:34 +0900

    loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,36 +1,37 @@
 'use strict';
 
-export const loader = function (...argv){
+export const loader = function (jsURL, fn, option){
 	/**
 	 * Worker does not support.
 	 * The only parent element is document.head.
 	 * 
-	 * arguments : [modules...|function, function, modules..., function]
+	 * jsURL : url string, Array or String
+	 * fn : function
+	 * option : TO-DO
 	 */
 
-	
-	let train = Array();
-	argv.forEach((entry, idx) => {
-		if (this.util.isArray(entry)) {
-			train=train.concat(entry)
-		} else train.push(entry)
-	});
+	jsURL = this.util.isArray(jsURL) ? jsURL : Array(jsURL);
+	let train = Array(), complete = Array(), next = (this.util.isFunction(fn) && fn) || this.util.noop;
+
 	const location_src = (src,cache) => {
 		let returnVal = src, d=/\?/.test(src);
-		returnVal += (cache? (d?'&':'?') + 't='+this.util.dateFormat(new Date(), 'yyyymmddss'):'');
+		returnVal += (cache ? '' : (d?'&':'?') + 't='+this.util.dateFormat(new Date(), 'yyyymmddss'));
 		return returnVal
 	};
-	const onload = (e) => {}
-	const load = (src) => {
-		if (!src) return;
-		const script = this.util.createElement({
+
+	const load = (entry) => {
+		if (!entry.url) return;
+		this.util.createElement({
 			dom:"script",
 			attr:{
-				src:location_src(src,true),
+				src:location_src(entry.url, false),
 				charset:'utf-8',
 				type:'text/javascript'
 			},
 			event:{
+				load: function (e) {
+					entry.complete = true;
+				},
 				error:function (e) {
 					throw new Error(src + ' : script loading error')
 				}
@@ -39,9 +40,21 @@ export const loader = function (...argv){
 		})
 	};
 
+	jsURL.forEach((url, idx) => {
+		complete.push(false);
+		train.push(this.util.object({index: idx, url:url}, {
+			complete : {
+				set : function (val) {
+					complete[this.index] = val;
+					if (complete.indexOf(false) === -1) {
+						next()
+					}
+				}
+			}
+		}))
+	});
+
 	train.forEach((entry, idx) => {
-		if (!entry) return false
-		if (this.util.isFunction(entry)) return false //TO-DO
 		load(entry)
 	});
 }
